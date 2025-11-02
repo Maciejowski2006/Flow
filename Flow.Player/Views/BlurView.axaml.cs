c4e3f4132c7e2d7fa3141ec22590bd0a485a2996
Maciej Chmiel <maciejowski2006yt@gmail.com>
2025-11-02 16:00:54 +0100

    Moved blur to a view

---
+++ Flow.Player/Views/BlurView.axaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace Flow.Player.Views;
+
+public partial class BlurView : UserControl
+{
+	public BlurView()
+	{
+		InitializeComponent();
+
+		bool isLinux = OperatingSystem.IsLinux();
+		NonLinuxBlur.IsVisible = !isLinux;
+		LinuxBlur.IsVisible = isLinux;
+	}
+}
