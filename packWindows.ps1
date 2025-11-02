a38dde8ba067b0ab7349a84aec84a4400b2973c2
Maciej Chmiel <maciejowski2006yt@gmail.com>
2025-11-02 15:15:16 +0100

    Added pack script for Windows

---
+++ packWindows.ps1
@@ -0,0 +1,7 @@
+﻿if ($args[0] -eq $null)
+{
+    Write-Host "Version is null. Set version as first argument (eg. ./packWindows.ps1 1.0.0)"
+    return
+}
+
+vpk pack --packId Flow --packVersion $args[0] --packDir .\Flow.Player\bin\Release\net8.0\win-x64\publish --mainExe Flow.Player.exe
